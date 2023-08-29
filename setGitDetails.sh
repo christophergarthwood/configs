4990f494ba3ac21598561a7d5b80ffb459bafb10
christopher.wood <christopher.wood@nrlssc.navy.mil>
2023-08-29 08:23:30 -0500

    SetGitDetails.

---
+++ setGitDetails.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+whoami_email="christopher.g.wood@gmail.com";
+whoami_name="Christopher G Wood";
+
+git config --global user.email "${whoami_email}"
+git config --global user.name "${whoami_name}"
