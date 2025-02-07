aae540b2ebaad3a5f35fa16693e72c8458b56838
christopher.wood <christopher.wood@nrlssc.navy.mil>
2025-02-07 14:42:50 -0600

    Added minimal scripts for jupyter environment restart.

---
+++ mountGCS-checkpoint.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/bash
+
+#Add this to your .bashrc or equivalent so you can `source ~/.bashrc` and receive the benefit of gs-fuse anytime.
+#References:
+#    https://cloud.google.com/blog/topics/developers-practitioners/cloud-storage-file-system-vertex-ai-workbench-notebooks/
+#    https://cloud.google.com/storage/docs/cloud-storage-fuse/cli-options
+
+export EXE_GCSFUSE="/usr/bin/gcsfuse";
+export GCS_BUCKET="general-335aun";
+export GCS_MOUNT_POINT="/home/jupyter/projects/gcs";
+
+cd ~/ # This should take you to /home/jupyter/
+
+#is the mount point present?
+if ! [ -d "${GCS_MOUNT_POINT}" ];
+then
+    # Create a folder that will be used as a mount point
+    status=$( mkdir -p "${GCS_MOUNT_POINT}" );
+
+    #was the directory creation successful?
+    if [ "${status}" != "0" ];
+    then
+      echo "ERROR: Status of ${status} returned.  Investigate attempts to create the (${GCS_MOUNT_POINT}) directory.";
+      exit 1;
+    fi
+fi
+
+#mount the file system
+status=$( gcsfuse --implicit-dirs --rename-dir-limit=100 --max-conns-per-host=100 "${GCS_BUCKET}" "${GCS_MOUNT_POINT}" );
+#status=$( ${EXE_GCSFUSE} "${GCS_BUCKET}" "${GCS_MOUNT_POINT}" );
+echo "Mount Status of:";
+echo "------------------------------------------------";
+echo " ${status}";
+echo "------------------------------------------------";
+
+#ls -alFh "${GCS_MOUNT_POINT}"
---
+++ pullPrivateInternalREPOS.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+#this code is to support using GitHub Command Line Utilities (gh) and Internal/Private repositories which require an additional layer of security.
+
+#repository names pulled from Repo->Code->HTTPS and remove the https:// at the start of the repo
+export repos=(code.fs.usda.gov/forest-service/CIO_CDO_Collaboration.git code.fs.usda.gov/forest-service/CIO_DataCalls.git)
+
+#create a GitHub Personal Access Token (PAT) via https://code.fs.usda.gov/settings/tokens using the Tokens (classic), remember this value and store it somewhere.
+export PAT="${GITHUB_PAT}"
+
+#find your USERNAME from your profile in code.fs.usda.gov
+export USERNAME="Christopher-Wood3"
+
+for repo in ${repos[@]}
+do
+  echo "Cloning ${repo}"
+  #Reference: https://stackoverflow.com/questions/2505096/clone-a-private-repository-github
+  git clone https://"${USERNAME}":"${PAT}"@${repo}
+
+done
